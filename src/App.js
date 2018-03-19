2488780614b464cd3f30330a4be297ce649eb5a3
Joseph Dalmasso <me@jodalmasso.com>
2018-03-19 19:13:03 +0100

    working on librarylist

--- src/App.js
+++ src/App.js
@@ -2,10 +2,16 @@ import React from 'react';
 import { View } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import reducers from './reducers';
+import { Header } from './components/common';
+import LibraryList from './components/LibraryList';
 
 const App = () => (
-  <Provider store={createStore}>
-    <View />
+  <Provider store={createStore(reducers)}>
+    <View>
+      <Header headerText="Tech Stack" />
+      <LibraryList />
+    </View>
   </Provider>
 );
 
